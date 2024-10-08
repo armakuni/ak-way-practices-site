148a3169db7e8f4f1e5ff3c79beb4df57ff2a6d1
Billie Thompson <billie.thompson@armakuni.com>
2024-10-08 16:20:41 +0200

    fix: Correct the url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Armakuni Way`,
-    siteUrl: `https://practices.armauki.com`,
+    siteUrl: `https://way.armauki.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
